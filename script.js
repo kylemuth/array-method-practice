8cee0cc56496f8feb39fd867aa49e79739713588
kylemuth <muthkyle1@gmail.com>
2022-12-08 18:13:29 -0500

    added more practice problems

--- script.js
+++ script.js
@@ -1,51 +1,199 @@
-const people = [
-  { name: "Kyle", age: 28, height: 72 },
-  { name: "John", age: 39, height: 68 },
-  { name: "Kevin", age: 19, height: 70 },
-  { name: "Jones", age: 27, height: 65 },
+// const people = [
+//   { name: "Kyle", age: 28, height: 72 },
+//   { name: "John", age: 39, height: 68 },
+//   { name: "Kevin", age: 19, height: 70 },
+//   { name: "Jones", age: 27, height: 65 },
+// ];
+
+// // Map will return individual values (unlike filter that will return the entire object) in the form of an array
+// const names = people
+//   .map((person) => {
+//     return person.name;
+//   })
+//   .join(", ");
+
+// document.querySelector("#names").insertAdjacentHTML("beforeend", ` ${names}`);
+
+// console.log(names);
+
+// /////////////////////////////////////////////////////////
+
+// // Filter Method: uses boolean logic to determine if an element meets a certain criterea. The returned values will be the entire object in which the criteria was met
+
+// const ages = people.filter((person) => {
+//   return person.age >= 28;
+// });
+
+// document
+//   .querySelector("#ages")
+//   .insertAdjacentHTML(
+//     "beforeend",
+//     ` ${ages[0].name} and ${ages[1].name} are older than 28`
+//   );
+
+// console.log(ages);
+
+// /////////////////////////////////////////////////////////
+
+// // Reduce Method: Does what it says: it reduces down elements into a single value.
+
+// const totalHeight = people.reduce((total, person) => {
+//   return total + person.height;
+// }, 0);
+
+// const averageHeight = totalHeight / people.length;
+
+// console.log(totalHeight);
+// console.log(averageHeight);
+
+// document
+//   .querySelector("#height")
+//   .insertAdjacentHTML("beforeend", ` ${averageHeight} inches`);
+
+//////////////////////////////////////////////////
+
+const inventors = [
+  { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+  { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+  { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+  { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+  { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+  { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+  { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+  { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+  { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+  { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+  { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+  { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
 ];
 
-// Map will return individual values (unlike filter that will return the entire object) in the form of an array
-const names = people
-  .map((person) => {
-    return person.name;
-  })
-  .join(", ");
+const peoples = [
+  "Bernhard, Sandra",
+  "Bethea, Erin",
+  "Becker, Carl",
+  "Bentsen, Lloyd",
+  "Beckett, Samuel",
+  "Blake, William",
+  "Berger, Ric",
+  "Beddoes, Mick",
+  "Beethoven, Ludwig",
+  "Belloc, Hilaire",
+  "Begin, Menachem",
+  "Bellow, Saul",
+  "Benchley, Robert",
+  "Blair, Robert",
+  "Benenson, Peter",
+  "Benjamin, Walter",
+  "Berlin, Irving",
+  "Benn, Tony",
+  "Benson, Leana",
+  "Bent, Silas",
+  "Berle, Milton",
+  "Berry, Halle",
+  "Biko, Steve",
+  "Beck, Glenn",
+  "Bergman, Ingmar",
+  "Black, Elk",
+  "Berio, Luciano",
+  "Berne, Eric",
+  "Berra, Yogi",
+  "Berry, Wendell",
+  "Bevan, Aneurin",
+  "Ben-Gurion, David",
+  "Bevel, Ken",
+  "Biden, Joseph",
+  "Bennington, Chester",
+  "Bierce, Ambrose",
+  "Billings, Josh",
+  "Birrell, Augustine",
+  "Blair, Tony",
+  "Beecher, Henry",
+  "Biondo, Frank",
+];
 
-document.querySelector("#names").insertAdjacentHTML("beforeend", ` ${names}`);
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
 
-console.log(names);
+const fifteen = inventors.filter(
+  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+);
 
-/////////////////////////////////////////////////////////
+console.table(fifteen);
 
-// Filter Method: uses boolean logic to determine if an element meets a certain criterea. The returned values will be the entire object in which the criteria was met
+// Array.prototype.map()
+// 2. Give us an array of the inventors first and last names
 
-const ages = people.filter((person) => {
-  return person.age >= 28;
-});
+const fullNames = inventors.map(
+  (inventor) => inventor.first + " " + inventor.last
+);
 
-document
-  .querySelector("#ages")
-  .insertAdjacentHTML(
-    "beforeend",
-    ` ${ages[0].name} and ${ages[1].name} are older than 28`
-  );
+console.table(fullNames);
 
-console.log(ages);
+// Array.prototype.sort()
+// 3. Sort the inventors by birthdate, oldest to youngest
 
-/////////////////////////////////////////////////////////
+const ordered = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
 
-// Reduce Method: Does what it says: it reduces down elements into a single value.
+console.table(ordered);
 
-const totalHeight = people.reduce((total, person) => {
-  return total + person.height;
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live all together?
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
 }, 0);
 
-const averageHeight = totalHeight / people.length;
+console.log(totalYears);
 
-console.log(totalHeight);
-console.log(averageHeight);
+// 5. Sort the inventors by years lived
 
-document
-  .querySelector("#height")
-  .insertAdjacentHTML("beforeend", ` ${averageHeight} inches`);
+const oldest = inventors.sort(function (a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+
+console.table(oldest);
+
+// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// 7. sort Exercise
+// Sort the people alphabetically by last name
+
+const alpha = peoples.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(", ");
+  const [bLast, bFirst] = nextOne.split(", ");
+  return aLast > bLast ? 1 : -1;
+});
+
+console.table(alpha);
+
+// 8. Reduce Exercise
+// Sum up the instances of each of these
+const data = [
+  "car",
+  "car",
+  "truck",
+  "truck",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "bike",
+  "walk",
+  "car",
+  "van",
+  "car",
+  "truck",
+];
+
+const transportation = data.reduce(function (obj, item) {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(transportation);
